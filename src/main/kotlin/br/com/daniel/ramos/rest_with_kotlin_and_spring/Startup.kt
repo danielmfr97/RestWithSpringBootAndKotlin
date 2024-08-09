9e9fb76a2b24aab4e61085c8b28d54549205f87f
danie <danielmfr6@gmail.com>
2024-08-09 06:24:53 -0300

    first commit

---
+++ src/main/kotlin/br/com/daniel/ramos/rest_with_kotlin_and_spring/Startup.kt
@@ -0,0 +1,11 @@
+package br.com.daniel.ramos.rest_with_kotlin_and_spring
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RestWithKotlinAndSpringApplication
+
+fun main(args: Array<String>) {
+	runApplication<RestWithKotlinAndSpringApplication>(*args)
+}
---
+++ src/test/kotlin/br/com/daniel/ramos/rest_with_kotlin_and_spring/StartupTest.kt
@@ -0,0 +1,13 @@
+package br.com.daniel.ramos.rest_with_kotlin_and_spring
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StartupTest {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
